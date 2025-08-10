260030a1184710f25ab577eb5383607dd432eab9
buehara <buehara@student.42sp.org.br>
2025-08-10 20:17:22 -0300

    update: ft_printf.h libraries

--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,13 @@
 /*   By: buehara <buehara@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 21:15:25 by buehara           #+#    #+#             */
-/*   Updated: 2025/08/10 17:05:20 by buehara          ###   ########.fr       */
+/*   Updated: 2025/08/10 20:16:18 by buehara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stddef.h>
-# include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
 
